bd0bad79315b9e480c7b9ad3f8256fcf10e2ee73
Pongnarin Tawan <pongnarin.thawan@gmail.com>
2017-05-22 16:05:44 +0700

    Add wrapper BackgroundLogo

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,23 @@ import SpeakersSection from '../components/SpeakersSection'
 import TicketsSection from '../components/TicketsSection'
 import AboutSection from '../components/AboutSection'
 import SponsorsSection from '../components/SponsorsSection'
+import BackgroundLogo from '../hocs/createBackgroundLogo'
 
 export default () => (
   <MainLayout>
     <HeroUnit />
     <SectionSeparator first />
-    <section id='about'><AboutSection /></section>
-    <SectionSeparator />
-    <section id='tickets'><TicketsSection /></section>
-    <SectionSeparator />
-    <section id='sponsors'><SponsorsSection /></section>
-    <SectionSeparator />
-    <section id='speakers'><SpeakersSection /></section>
+    <BackgroundLogo position='right'>
+      <section id='about'><AboutSection /></section>
+      <SectionSeparator />
+      <section id='tickets'><TicketsSection /></section>
+      <SectionSeparator />
+      <section id='sponsors'><SponsorsSection /></section>
+      <SectionSeparator />
+    </BackgroundLogo>
+    <BackgroundLogo>
+      <section id='speakers'><SpeakersSection /></section>
+    </BackgroundLogo>
     <SectionSeparator />
     <section id='contact'><FooterSection /></section>
   </MainLayout>
